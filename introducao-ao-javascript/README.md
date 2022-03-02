03ee400346002b61df465a7fd7079b27949946ab
Thiago <thiago_andre_baader@hotmail.com>
2022-03-02 17:24:17 -0300

    initial commit

---
+++ introducao-ao-javascript/README.md
@@ -0,0 +1,6 @@
+# Introdução ao Javascript
+
+Projetos referentes ao curso "Introdução ao Javascript", que faz parte do Take Blip Web Developer Bootcamp da Digital Innovation One.
+
+- To-do List
+- Contador
---
+++ resolvendo-desafios-em-javascript/nossos-dias-nunca-voltarão/nossos-dias-nunca-voltarao.js
@@ -0,0 +1,7 @@
+let lines = gets().split("\n");
+
+let n = parseInt(lines.shift());
+//escreva o seu código aqui
+let phrase = "life is not a problem to be solved"
+
+console.log(phrase.slice(0, n).toUpperCase());
